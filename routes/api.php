f30d1456955f01a9bfda3723fc2285b28f4bb173
Ezequiel Flores <ezequiel@proz.com>
2019-08-15 14:08:05 -0300

    return collection of User model from database

--- routes/api.php
+++ routes/api.php
@@ -13,13 +13,7 @@ use Illuminate\Http\Request;
 |
  */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
 
-Route::get('/users', function () {
-    if (rand(1, 10) < 3) {
-        abort(500, 'We could not retrieve the users');
-    }
-    return factory('App\User', 10)->make();
+Route::namespace('Api')->group(function () {
+    Route::get('/users', 'UsersController@index');
 });
